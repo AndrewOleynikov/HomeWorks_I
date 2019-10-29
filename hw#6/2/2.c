ecd0c1041e011c2007ee882e4502fa48b9b6c61d
Andrew Oeynikov <a.oleyn1kov@outlook.com>
2019-10-29 17:58:24 +0300

    Hw #6

---
+++ hw#6/2/2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+int main()
+{
+    const size_t N = 50;
+    char str[N];
+    for (int i = 0; i < N; i++) {
+        str[i] = NULL;
+    }
+    printf("%s","write number: ");
+    scanf("%s", str);
+    int correct = 1;
+    int sign = 0;
+    int i = 0;
+    if (str[0] == '-') {
+        sign = 1;
+        i = 1;
+    }
+    unsigned ans = 0;
+    for (; i < strlen(str); i++) {
+        if (str[i] < '0' || str[i] > '9') {
+            printf("%s", "Error input");
+            correct = 0;
+            break;
+        }
+        int numb = str[i] - '0';
+      //  printf("%d", numb);
+        ans = ans * 10 + numb;
+        if (ans > INT_MAX) {
+            correct = 0;
+            printf("%s", "Error overflow");
+            break;
+        }
+    }
+    if (correct == 1) {
+            printf("%d", ans * (-1));
+        if (sign) {
+        } else {
+            printf("%d5", ans);
+        }
+    }
+
+    return 0;
+}
