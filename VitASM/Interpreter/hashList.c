777891cdf89ddab5b1e8a29b5f7b77ddf2f83803
Andrew Oeynikov <a.oleyn1kov@outlook.com>
2020-02-28 17:09:52 +0300

    VitASM Interpreter

---
+++ VitASM/Interpreter/hashList.c
@@ -0,0 +1,310 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <limits.h>
+#include "hashList.h"
+
+struct Node* createNode(int value, char *word)
+{
+    struct Node *node = (struct Node*)malloc(sizeof(struct Node));
+    if (!node)
+    {
+        printf("ERRORm\n");
+        return NULL;
+    }
+    node->value = value;
+    node->next = NULL;
+    strcpy(node->word, word);
+    return node;
+}
+
+struct List* createList()
+{
+    struct List *list = (struct List*)malloc(sizeof(struct List));
+    if (!list)
+    {
+        printf("ERRORm\n");
+        return NULL;
+    }
+    list->head = NULL;
+    return list;
+}
+
+void addHead(struct List *list, int value, char *word)
+{
+    struct Node *node = createNode(value, word);
+    node->next = list->head;
+    list->head = node;
+}
+
+void addTail(struct List *list, int value, char *word)
+{
+   /// struct Node *node = (struct Node*)malloc(sizeof(struct Node));-------------------ОШИБКА
+    /*if (list->head)///!node NNN
+    {
+        struct Node *node = (struct Node*)malloc(sizeof(struct Node));
+        printf("ERRORm\n");
+        return;
+    }*/
+    if (list->head)
+    {
+        struct Node *node = (struct Node*)malloc(sizeof(struct Node));///-----------ИСПРАВЛЕНИЕ
+        if (!node)
+        {
+            printf("ERRORm\n");
+            return;
+        }
+        node = list->head;
+        /*if (strcmp(node->word, word) == 0 && node->value)
+        {
+            node->value++;
+            return;
+        }*/
+        while (node->next)
+        {
+            /*if (strcmp(node->word, word) == 0 && node->value)
+            {
+                node->value++;
+                return;
+            }*/
+            node = node->next;
+        }
+       /* if (strcmp(node->word, word) == 0 && node->value)
+        {
+            node->value++;
+            return;
+        }*/
+        node->next = createNode(value, word);
+        strcpy(node->next->word, word);
+    } else {
+        list->head = createNode(value, word);
+    }
+}
+
+void addAfter(struct List *list, int after, int value, char *word)
+{
+    struct Node *node = list->head;
+    while (node)
+    {
+        if (node->value == after)
+        {
+            struct Node* newNode = createNode(value, word);
+            newNode->next = node->next;
+            node->next = newNode;
+            strcpy(node->word, word);
+            break;
+        }
+        node = node->next;
+    }
+}
+
+void deleteNode(struct List *list, char *word)
+{
+    if (list->head->next)
+    {
+        struct Node *before = (struct Node*)malloc(sizeof(struct Node));
+        if (!before)
+        {
+            printf("ERRORm\n");
+            return;
+        }
+        strcpy(before->word, list->head->word);
+        before = list->head;
+        struct Node *after = (struct Node*)malloc(sizeof(struct Node));
+        if (!after)
+        {
+            printf("ERRORm\n");
+            return;
+        }
+        strcpy(after->word, list->head->word);
+        after = list->head->next;
+        while (after)
+        {
+            if (!strcmp(after->word, word))
+            {
+                struct Node *toDelete = (struct Node*)malloc(sizeof(struct Node));
+                if (!toDelete)
+                {
+                    printf("ERRORm\n");
+                    return;
+                }
+                toDelete = after;
+               /* if (after->value > 1)
+                {
+                    after->value--;
+                    return;
+                }*/
+                if (toDelete->next)
+                {
+                    after = toDelete->next;
+                    before->next = after;
+                    free(toDelete);
+                } else {
+                    after = NULL;
+                    before->next = after;
+                    free(toDelete);
+                }
+
+            } else {
+                after = after->next;
+                before= before->next;
+            }
+        }
+    } else {
+        if (strcmp(list->head->word, word) == 0)
+        {
+            /*if (list->head->value > 1){
+                list->head->value--;
+                return;
+            }*/
+            struct Node *toDelete = (struct Node*)malloc(sizeof(struct Node));
+            if (!toDelete)
+            {
+                printf("ERRORm\n");
+                return;
+            }
+            toDelete->next = NULL;
+            list->head = toDelete->next;
+            free(toDelete);
+            return;
+        }
+    }
+    if (strcmp(list->head->word, word) == 0)
+    {/*
+        if (list->head->value > 1)
+        {
+            list->head->value--;
+            return;
+        }*/
+        struct Node *toDelete = (struct Node*)malloc(sizeof(struct Node));
+        if (!toDelete)
+        {
+            printf("ERRORm\n");
+            return;
+        }
+
+        toDelete = list->head;
+        list->head = toDelete->next;
+        free(toDelete);
+    }
+}
+
+void eraseList(struct List *list)
+{
+    struct Node *node = list->head;
+    while (node)
+    {
+        struct Node *toDelete = node;
+        node = node->next;
+        free(toDelete);
+    }
+    list->head = NULL;
+    free(list);
+}
+
+void makeCycle(struct List *list, int from, int to)
+{
+    if (!list->head->next)
+    {
+        printf("Need more elements\n");
+        return;
+    } else {
+        struct Node *node = list->head;
+        struct Node *nodeFrom = NULL;
+        while (node->next)
+        {
+            if (node->value == from)
+            {
+                nodeFrom = node;
+            }
+            if (node->value == to && nodeFrom)
+            {
+                node->next = nodeFrom;
+                return;
+            }
+            node = node->next;
+        }
+        if ( node->value == to)
+        {
+            node->next = nodeFrom;
+        }
+    }
+}
+
+int haveLoop(struct List *list)
+{
+    struct Node *one = createNode(list->head->value, " ");
+    struct Node *two = createNode(list->head->value, " ");
+    while(1)
+    {
+        if ((one->next == NULL) || (two->next->next == NULL))
+        {
+            return 0;
+        }
+        one = one->next;
+        two = two->next->next;
+        if (one == two)
+        {
+            return 1;
+        }
+    }
+    free(one);
+    free(two);
+}
+
+size_t listSize(struct List *list)
+{
+    if (list->head)
+    {
+        struct Node *node = list->head;
+        size_t size = 0;
+        while (node->next)
+        {
+            size++;
+            node = node->next;
+        }
+        if (node)
+        {
+            return size + 1;
+        }
+        return 0;
+    }
+}
+
+size_t countIndividuals(struct List *list)
+{
+    if (list->head)
+    {
+        size_t ans = 0;
+        struct Node *node = list->head;
+        while (node->next)
+        {
+            ans++;
+            node = node->next;
+        }
+        return ans + 1;
+    } else {
+        return 0;
+    }
+}
+
+void printList(struct List *list)
+{
+    if (list->head && !haveLoop(list))
+    {
+        struct Node *node = list->head;
+        while (node->next)
+        {
+            printf("%d %s\n", node->value, node->word);
+            node = node->next;
+
+        }
+        printf("%d %s\n", node->value, node->word);
+    }
+    if (list->head && haveLoop(list))
+    {
+        printf("List have cycle\n");
+    }
+}
---
+++ VitASM/Interpreter/hashTable.c
@@ -0,0 +1,122 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+#include "hashList.h"
+#include "hashTable.h"
+
+struct HashTable *createHashTable(size_t size, size_t(*fn)(char* word))
+{
+    struct HashTable *table = (struct HashTable*)malloc(sizeof(struct HashTable));
+
+    table->entries = (struct List*)malloc(sizeof(struct List) * size);
+    for (size_t i = 0; i < size; i++)
+    {
+       table->entries[i] = createList();
+    }
+    table->size = size;
+    table->fn = fn;
+    table->countElements = 0;
+    return table;
+}
+
+
+void deleteHashTable(struct HashTable *table)
+{
+    for (size_t i = 0; i < table->size; i++)
+    {
+        eraseList(table->entries[i]);
+    }
+    free(table->entries);/// ОШИБКА (ОТСУТСТВИЕ ЭТОЙ ОПЕРАЦИИ)
+    free(table);
+}
+
+void addToTable(struct HashTable *table, char *word, size_t numb)
+{
+    size_t it = table->fn(word);
+    it %= table->size;
+    addTail(table->entries[it], numb, word);
+    table->countElements++;
+}
+
+void deleteFromTable(struct HashTable *table, char *word)
+{
+    size_t it = table->fn(word);
+    it %= table->size;
+    deleteNode(table->entries[it], word);
+    table->countElements--;
+}
+
+int find(struct HashTable *table, char *word)
+{
+    size_t it = table->fn(word);
+    it %= table->size;
+    if (!table->entries[it]->head)
+    {
+        return 0;
+    }
+    struct Node *node = table->entries[it]->head;
+    while (node)
+    {
+        if (strcmp(node->word, word) == 0)
+        {
+            return node->value;
+        }
+        node = node->next;
+    }
+    return 0;
+}
+
+void printHashTable(struct HashTable *table)
+{
+    for (size_t i = 0; i < table->size; i++)
+    {
+        printList(table->entries[i]);
+    }
+}
+
+void showTableInfo(struct HashTable *table)
+{
+    printf("\n\n");
+    size_t countUnempty = 0;
+    size_t countElem = 0;
+    size_t middleLength = 0;
+    size_t minList = INT_MAX;
+    size_t maxList = 0;
+    size_t sum = 0;
+    size_t countInd = 0;
+    for (size_t i = 0; i < table->size; i++)
+    {
+        printList(table->entries[i]);
+        size_t h = listSize(table->entries[i]);
+        countInd += countIndividuals(table->entries[i]);
+        if (h)
+        {
+            sum += h;
+            countUnempty++;
+        }
+        if (maxList < h)
+        {
+            maxList = h;
+        }
+        if (minList > h)
+        {
+            minList = h;
+        }
+    }
+
+    printf("Count of all elements: %d\n", table->countElements);
+    printf("Count of different elements: %d\n", countInd);
+    printf("Count of unemptyes slots: %d\n", countUnempty);
+    printf("Middle value of length of chain: %d\n", sum / countUnempty);
+    printf("Max length of chain: %d\n", maxList);
+    if (minList < INT_MAX)
+    {
+        printf("Min length of chain: %d\n", minList);
+    } else {
+        printf("Min length of chain: %d\n", 0);
+    }
+}
